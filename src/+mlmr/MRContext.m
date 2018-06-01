db3860f0c4781d8eb6ae2001ed801451ab829a50
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2018-06-01 01:00:13 -0500

    Checking in before work on mlsiemens.Herscovitch1985.

---
+++ src/+mlmr/MRContext.m
@@ -0,0 +1,25 @@
+classdef MRContext 
+	%% MRCONTEXT  
+
+	%  $Revision$
+ 	%  was created 28-May-2018 21:46:13 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlmr/src/+mlmr.
+ 	%% It was developed on Matlab 9.4.0.813654 (R2018a) for MACI64.  Copyright 2018 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+ 		function this = MRContext(varargin)
+ 			%% MRCONTEXT
+ 			%  @param .
+
+ 			
+ 		end
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
